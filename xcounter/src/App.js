e1ec0437896e02491691fdbeefb6cf81a6a22458
Priyanka Chaudhari <cpriyanka71m@gmail.com>
2025-07-08 16:50:54 +0530

    Number Counter

---
+++ xcounter/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter App</h1>
+      <div className="count-display">Count: {count}</div>
+      <div className="buttons">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
